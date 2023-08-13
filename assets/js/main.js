f23b17709687a6902b7842225427182b87592c3d
Naiem-Devs <dnsnaiem@gmail.com>
2023-08-13 16:02:19 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+  // owlCarousel
+  $(".info-slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 1,
+    navText: [
+      '<img src="assets/img/left.svg" class="" alt="">',
+      '<img src="assets/img/right.svg" class="" alt="">'
+    ],
+    nav: true,
+    smartSpeed: 1000,
+    autoplay: true,
+    autoplayTimeout:5000,
+    dots: false
+  });
+
+  
+
+      //test for getting url value from attr
+// var img1 = $('.test').attr("data-thumbnail");
+// console.log(img1);
+
+//test for iterating over child elements
+var langArray = [];
+$('.vodiapicker option').each(function(){
+  var img = $(this).attr("data-thumbnail");
+  var text = this.innerText;
+  var value = $(this).val();
+  var item = '<li><img src="'+ img +'" alt="" value="'+value+'"/><span>'+ text +'</span></li>';
+  langArray.push(item);
+})
+
+$('#a').html(langArray);
+
+//Set the button value to the first el of the array
+$('.btn-select').html(langArray[0]);
+$('.btn-select').attr('value', 'en');
+
+//change button stuff on click
+$('#a li').click(function(){
+   var img = $(this).find('img').attr("src");
+   var value = $(this).find('img').attr('value');
+   var text = this.innerText;
+   var item = '<li><img src="'+ img +'" alt="" /><span>'+ text +'</span></li>';
+  $('.btn-select').html(item);
+  $('.btn-select').attr('value', value);
+  $(".b").toggle();
+  //console.log(value);
+});
+
+$(".btn-select").click(function(){
+        $(".b").toggle();
+    });
+
+//check local storage for the lang
+var sessionLang = localStorage.getItem('lang');
+if (sessionLang){
+  //find an item with value of sessionLang
+  var langIndex = langArray.indexOf(sessionLang);
+  $('.btn-select').html(langArray[langIndex]);
+  $('.btn-select').attr('value', sessionLang);
+} else {
+   var langIndex = langArray.indexOf('ch');
+  console.log(langIndex);
+  $('.btn-select').html(langArray[langIndex]);
+  //$('.btn-select').attr('value', 'en');
+}
+
+
+
+
+AOS.init({
+  mirror: true,
+  duration: 1500,
+  initClassName: 'aos-init',
+  once: true,
+});
+  
+})(jQuery);
